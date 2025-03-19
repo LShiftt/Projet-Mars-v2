8795332ad77892d4e3a1b413aec5ad2ae0e16319
TwisT404 <cedric.tanghe1303@gmail.com>
2025-03-19 18:57:28 +0100

    fix print reward

--- js/game/question/challenge.js
+++ js/game/question/challenge.js
@@ -26,7 +26,7 @@ function printMissions() {
                     <p>${event.description}</p>
                 </div>
                 <p>${parseInt(storedValue)} / ${event.objectif}</p>
-                <p>Récompense: <p>
+                <p>Récompense: ${event.recompense}<p>
             </div>
         `;
         }
